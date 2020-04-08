dc1b20c8aecfdaf81ed6f516b75f4ee0d4e3fe65
Rafael França <rafaelfldasilva@gmail.com>
2020-04-08 16:17:21 -0300

    add routes and fix pages names add default imports

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Import das paginas
+import Login from "./src/Login/Login"
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Login/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
